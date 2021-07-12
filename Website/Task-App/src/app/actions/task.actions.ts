198729727c1ec15d9781d12ec76a1e05cbf5ab2b
crspradlin <mail@crspradlin.org>
2021-07-11 21:48:59 -0400

    tried to update docker and some settings

--- Website/Task-App/src/app/actions/task.actions.ts
+++ Website/Task-App/src/app/actions/task.actions.ts
@@ -2,14 +2,14 @@ import { createAction, props } from "@ngrx/store";
 import { TaskEntity } from "../reducers/tasks.reducer";
 
 export const loadTasks = createAction(
-    '[app task] Loading task data'
+    '[app task] Loading Task Data'
 );
 
 export const clearTasks = createAction(
-    '[app task] Clear task data'
+    '[app task] Clear Task Data'
 );
 
 export const tasksSuccessfullyLoaded = createAction(
-    '[app task] Task data successfully loaded',
+    '[app task] Task Data Successfully Loaded',
     props<{ payload: TaskEntity[] }>()
 )
